d83cdca2cf62db19ccbbe0fd28b5081714ecf218
Gabrielito666 <galileo.fandango@gmail.com>
2024-03-07 22:25:49 -0300

    1.0.2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
 
 const imageJsContent =
 `import React from "react";
@@ -26,34 +27,42 @@ const Image = _ =>
 
 export { Image, settings };`
 
-const stylesCssPath = path.join(process.cwd(), 'styles.css');
-const packageJsonPath = path.join(process.cwd(), 'package.json');
-const imageJsPath = path.join(process.cwd(), 'image.js');
-
-fs.readFile(packageJsonPath, (err, data) => {
-  if (err) throw err;
-  const packageJson = JSON.parse(data);
-
-  const scriptsToAdd = {
-    "start:react": `webpack serve --mode development --entry ${path.resolve(__dirname, './previewer.js')} --no-open`,
-    "start:electron": `electron ${path.resolve(__dirname, './electron-starter.js')}`,
-    "dev": "concurrently \"npm run start:react\" \"wait-on http://localhost:8080 && npm run start:electron\"",
-    "export": `node ${path.resolve(__dirname, './capture.js')}`
-  };
-
-  packageJson.scripts = {...packageJson.scripts, ...scriptsToAdd};
-
-  fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), (err) => {
+const addScriptsToPackageJson = () => {
+  fs.readFile(packageJsonPath, (err, data) => {
     if (err) throw err;
+    const packageJson = JSON.parse(data);
+
+    const scriptsToAdd = {
+      "start:react": `webpack serve --mode development --entry ${path.resolve(__dirname, './previewer.js')} --no-open`,
+      "start:electron": `electron ${path.resolve(__dirname, './electron-starter.js')}`,
+      "dev": "concurrently \"npm run start:react\" \"wait-on http://localhost:8080 && npm run start:electron\"",
+      "export": `node ${path.resolve(__dirname, './capture.js')}`
+    };
+
+    packageJson.scripts = { ...packageJson.scripts, ...scriptsToAdd };
+
+    fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), (err) => {
+      if (err) throw err;
+      console.log('Scripts added to package.json successfully.');
+    });
   });
-});
+}
+
+if (!fs.existsSync(packageJsonPath)) {
+  console.log('npm init...');
+  execSync('npm init -y', { stdio: 'inherit' });
+  addScriptsToPackageJson();
+} else {
+  addScriptsToPackageJson();
+}
+
 fs.writeFile(imageJsPath, imageJsContent, () => {});
-fs.writeFile(stylesCssPath, '',() => {});
+fs.writeFile(stylesCssPath, '', () => {});
 
 console.log(`
-your "lek-photographic-studio" project has been successfully initialized.
+Your "lek-photographic-studio" project has been successfully initialized.
 
-use 'npm run dev' to open the previewer and edit image.js to edit your photo.
+Use 'npm run dev' to open the previewer and edit image.js to edit your photo.
 
-use 'npm run export' to export the image to where you want it to go`
-)
+Use 'npm run export' to export the image to where you want it to go.
+`);
