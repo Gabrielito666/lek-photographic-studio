57a2f4a434442d17289d3e4efed5eb826439258e
Gabrielito666 <galileo.fandango@gmail.com>
2024-03-10 23:35:15 -0300

    2.0.6

--- export/capture.js
+++ export/capture.js
@@ -1,11 +1,10 @@
 const puppeteer = require('puppeteer');
 const getExportHtml = require('./getExportHtml');
-
-const puppeteer = require('puppeteer');
+const path = require('path');
 
 (async () => {
   const { html, exports, id } = await getExportHtml();
-  const output = exports ? path(process.cwd(), exports) : path(process.cwd(), './output.png');
+  const output = exports ? path.resolve(process.cwd(), exports) : path.resolve(process.cwd(), './output.png');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
@@ -14,4 +13,5 @@ const puppeteer = require('puppeteer');
   const div = await page.$(`#${id}`);
   await div.screenshot({ path: output });
   await browser.close();
+  console.log('your image has been successfully exported')
 })();
--- export/getExportHtml.js
+++ export/getExportHtml.js
@@ -1,6 +1,7 @@
 const ReactDOMServer = require('react-dom/server');
 const React = require('react');
 const crypto = require('crypto');
+const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 const webpackConfig = require('lek-photographic-studio/export/webpack-export-config');
@@ -12,12 +13,19 @@ const getExportHtml = async () =>
   console.log('wait please...')
   await webpack_compile();
   const Image = require('lek-photographic-studio/export/dist-export/bundle');
+  const stringStyle = fs.readFileSync(path.resolve(__dirname, './dist-export/styles.css'), 'utf-8');
   const ImageOriginal = Image.default();
   const { width, height, exports, className, children } = ImageOriginal.props;
   const style = { width : width + 'px', height : height + 'px' };
 
-  const Component = () => React.createElement('div', { className, style, id }, children);
+  const Component = () => React.createElement(
+    'div', 
+    null,
+    React.createElement('style', null, stringStyle),
+    React.createElement('div', { className, style, id }, children)
+  );
   const html = ReactDOMServer.renderToStaticMarkup(Component());
+  console.log(html)
   return { html, exports, id };
 };
 
